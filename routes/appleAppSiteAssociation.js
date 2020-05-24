979942f156b1286fa80692c08b982775b08d192d
Erik Flores <erikfloresq@gmail.com>
2020-05-24 20:56:42 -0300

    Add pkcs7-mime

--- routes/appleAppSiteAssociation.js
+++ routes/appleAppSiteAssociation.js
@@ -4,6 +4,7 @@ const appleJson = require('../public/apple-app-site-association.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.set('Content-Type', 'application/pkcs7-mime')
   res.header("Content-Type",'application/json');
   res.json(appleJson);
 });
