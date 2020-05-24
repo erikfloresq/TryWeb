3c273c9d9fbe5fc21584a0aab51ecfd4ec188d2a
Erik Flores <erikfloresq@gmail.com>
2020-05-24 20:46:35 -0300

    Modify aasa router

--- routes/appleAppSiteAssociation.js
+++ routes/appleAppSiteAssociation.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const appleJson = require('../public/apple-app-site-association.json');
 
 /* GET users listing. */
-router.get('/apple-app-site-association', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.header("Content-Type",'application/json');
   res.json(appleJson);
 });
